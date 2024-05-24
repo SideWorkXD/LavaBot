aea1189b9e2907302319a44cb328372dbdbcf050
SideWorkXD <164920141+SideWorkXD@users.noreply.github.com>
2024-05-24 19:55:51 +0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,12 +9,27 @@ module.exports = {
     try {
       const embed = new EmbedBuilder()
         .setColor("#0099ff")
-        .setTitle("💎 Music Bot by BILLO")
-        .setDescription(
-          "Welcome to the Music Bot!\n\n- Here are the available commands:\n\n" +
-            "**/play :** Start playing the songs.\n" +
-            "**/ping :** check bot latency.\n" +
-            "**/support :** Display support server info",
+        .setTitle("<:SupportsCommands:1243577824477188197> **Basic Commands**")
+        .addFields(
+          {
+            name: "<a:All_connect:1230899360976470160> Ping",
+            value: "Check the bot's latency",
+          },
+          {
+            name: "<a:music:1241029884902641778> Play",
+            value: "Clear the song queue of this server",
+          },
+          {
+            name: "<:discordverify:861292720324476968> Support",
+            value: "Support Server Links",
+          },
+          {
+            name: "<a:utility:1241028673797099520> Help",
+            value: "Display all the helps command",
+          },
+        )
+        .setImage(
+          "https://media.discordapp.net/attachments/1155424116887797790/1222196420241653882/standard_1.gif?ex=6615561d&is=6602e11d&hm=24c48480bcee4c72ee4358ddb3b28142c9445a5496ec64f472240a55eeda6206&=",
         );
 
       return interaction.reply({ embeds: [embed] });
