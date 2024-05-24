883fec0ac313a0982595ebadaefb0d002096f388
SideWorkXD <164920141+SideWorkXD@users.noreply.github.com>
2024-05-24 20:00:13 +0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,24 +1,4 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-const { EmbedBuilder } = require('discord.js')
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "ping",
@@ -26,42 +6,23 @@ module.exports = {
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
-
-
     try {
-
       const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
-        const end = Date.now();
-        const embed = new EmbedBuilder()
-          .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
-          .setDescription(`**Pong** : ${end - start}ms`)
-        return interaction.editReply({ embeds: [embed] }).catch(e => { });
-      }).catch(err => { })
-
+      interaction
+        .reply("Pinging....")
+        .then((msg) => {
+          const end = Date.now();
+          const embed = new EmbedBuilder()
+            .setColor(`#6190ff`)
+            .setTitle(`Bot Latency`)
+            .setDescription(
+              `<a:All_connect:1230899360976470160> **Pong** : ${end - start}ms`,
+            );
+          return interaction.editReply({ embeds: [embed] }).catch((e) => {});
+        })
+        .catch((err) => {});
     } catch (e) {
-    console.error(e); 
-  }
+      console.error(e);
+    }
   },
 };
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
